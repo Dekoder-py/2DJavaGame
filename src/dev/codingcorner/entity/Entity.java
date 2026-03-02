2392fa89701d52365da62667209137138735da13
Dekoder-py <kyle@codingcorner.dev>
2026-03-02 21:04:21 +1300

    add player sprite (todo: add different sprites based on direction)

--- src/dev/codingcorner/entity/Entity.java
+++ src/dev/codingcorner/entity/Entity.java
@@ -1,7 +1,12 @@
 package dev.codingcorner.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
     public int x, y;
     public int speed;
+
+    public BufferedImage sprite;
+    public String direction;
 }
--- src/dev/codingcorner/entity/Player.java
+++ src/dev/codingcorner/entity/Player.java
@@ -3,7 +3,10 @@ package dev.codingcorner.entity;
 import dev.codingcorner.core.GamePanel;
 import dev.codingcorner.core.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -15,6 +18,8 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
+        direction = "down";
     }
 
     public void setDefaultValues() {
@@ -24,25 +29,43 @@ public class Player extends Entity {
         speed = 4;
     }
 
+    public void getPlayerImage() {
+        try {
+            // TODO: add more sprite states (up1, up2, down1, down2, left1 ...) and load them as the names over there <-- also, video: https://youtu.be/wT9uNGzMEM4
+            sprite = ImageIO.read(getClass().getResourceAsStream("/player/player.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void update() {
 
         if (keyH.upPressed) {
             y -= speed;
+            direction = "up";
         }
         if (keyH.downPressed) {
             y += speed;
+            direction = "down";
         }
         if (keyH.leftPressed) {
             x -= speed;
+            direction = "left";
         }
         if (keyH.rightPressed) {
             x += speed;
+            direction = "right";
         }
     }
 
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        // TODO: add more sprite states (up1, up2, down1, down2, left1 ...) and use switch statement to set sprite; see: https://youtu.be/wT9uNGzMEM4
+        // switch (direction) {}
+
+        image = sprite;
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
