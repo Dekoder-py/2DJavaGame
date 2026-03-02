50261a8879fce12eba2f641a90903ac7553c895f
Dekoder-py <kyle@codingcorner.dev>
2026-03-02 17:16:17 +1300

    initial commit

---
+++ src/dev/codingcorner/core/Main.java
@@ -0,0 +1,18 @@
+package dev.codingcorner.core;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static String WINDOW_TITLE = "2D Adventure";
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle(WINDOW_TITLE);
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
